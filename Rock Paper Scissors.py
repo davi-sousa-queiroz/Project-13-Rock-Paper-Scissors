b9219581023f65f8fbdc328cf677a506b5c7899c
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-16 16:52:26 -0300

    commit new modified and efficient functions

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -29,5 +29,42 @@ def welcome():
     time.sleep(2)
     print("Good Luck!\n")
     time.sleep(1)
+def player_hand():
+    player_move = input("What's your move: (r)ock, (p)aper, or (s)cissors?\n>>")
+def computer_hand():
+    hands = ['r', 'p', 's']
+    computer_move = random.choice(hands)
+def win():
+    global wins
+    print("You won!")
+    wins += 1
+def lose():
+    global loses
+    print("You lost!")
+    loses += 1
+def tie():
+    global ties
+    print("You tied!")
+    ties += 1
+def calculate_winner(player_move, computer_move):
+    global wins
+    global loses
+    global ties
+    if player_move == computer_move:
+        tie()
+    elif player_move == 'r' and computer_move == 'p':
+        lose()
+    elif player_move == 'r' and computer_move == 's':
+        win()
+    elif player_move == 'p' and computer_move == 'r':
+        win()
+    elif player_move == 'p' and computer_move == 's':
+        lose()
+    elif player_move == 's' and computer_move == 'p':
+        win()
+    elif player_move == 's' and computer_move == 'r':
+        lose()
+    else:
+        print('GAME BUG IN CALCULATE WINNER FUNCTION')
 
 # ----------- MAIN LOOP -------------
